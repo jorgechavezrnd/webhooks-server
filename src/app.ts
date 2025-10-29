38397076aa7b54e26a80eb3994da080849f3e531
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-10-29 11:08:06 -0400

    Initial commit with base structure working

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import { envs } from './config';
+
+(() => {
+  main();
+})();
+
+function main() {
+  const app = express();
+
+  app.post('/api/github', (req, res) => {
+    res.json('Github Endpoint');
+  });
+
+  app.listen(envs.PORT, () => {
+    console.log(`App running on port ${ envs.PORT }`);
+  });
+}
