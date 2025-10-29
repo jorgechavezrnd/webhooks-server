1b6e1a3a079a77ccbc6bdc21f230080b9fec6bca
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-10-29 14:41:26 -0400

    Add integration with Discord webhook and send message to chanel

--- src/presentation/github/controller.ts
+++ src/presentation/github/controller.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
 import { GitHubService } from '../services/github.service';
+import { DiscordService } from '../services/discord.service';
 
 export class GithubController {
 
   constructor(
     private readonly githubService = new GitHubService(),
+    private readonly discordService = new DiscordService(),
   ) {}
 
   webhookHandler = (req: Request, res: Response) => {
@@ -25,9 +27,9 @@ export class GithubController {
         message = `Unknown event ${ githubEvent }`;
     }
 
-    console.log({message});
-
-    res.status(201).send('Accepted');
+    this.discordService.notify(message)
+      .then( () => res.status(202).send('Accepted') )
+      .catch( () => res.status(500).json({ error: 'internal server error' }) );
   };
 
 }
